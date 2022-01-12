603fdc0449b43937df1de92dd5933182312167df
first-phoenix <iswarajkhan@gmail.com>
2022-01-13 01:52:49 +0530

    Added files

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome To Docharity',
+  description: 'We help NGOs to raise fund',
+  keywords: 'NGO, Charity, Crowdfunding',
+}
+
+export default Meta
